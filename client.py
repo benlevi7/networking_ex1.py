21bdcc5f043dac54cb5ee434aa5dc916cf0113c9
leviben2 <benlevi7770528@gmail.com>
2021-10-29 16:11:02 -0700

    finish check arg - client

--- client.py
+++ client.py
@@ -1,26 +1,63 @@
 import socket
 import sys
-import time
 
-IP = sys.argv[1]
-PORT = int(sys.argv[2])
-FILE_NAME = sys.argv[3]
-DEFAULT_TIMEOUT = 12
+NUM_OF_ARG = 3
 SIZE_PKG = 97
-
-
+MAX_PORT = 65545
+MAX_IP = 255
+DEFAULT_TIMEOUT = 12
 c_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 c_socket.settimeout(DEFAULT_TIMEOUT)
 
 
-data = open(FILE_NAME, "rb").read()
-arr_data = [data[i:i + SIZE_PKG] for i in range(0, len(data), SIZE_PKG)]
+def off():
+    c_socket.close()
+    exit()
+
+
+def check_arg():
+    # IP + PORT + FILE_NAME (and program name).
+    if len(sys.argv) != NUM_OF_ARG + 1:
+        off()
+    try:
+        # Check valid IP address.
+        arr = sys.argv[1].split('.')
+        # IPv4
+        if len(arr) != 4:
+            off()
+        for n in arr:
+            if int(n) > MAX_IP or int(n) < 0:
+                off()
+
+        # Check port is valid int type.
+        if int(sys.argv[2]) > MAX_PORT or int(sys.argv[2]) < 0:
+            off()
+    except:
+        off()
+
+
+check_arg()
+IP = sys.argv[1]
+PORT = int(sys.argv[2])
+FILE_NAME = sys.argv[3]
+c_socket.bind(('', PORT))
+arr_data = []
+# Check file is exist.
+try:
+    data = open(FILE_NAME, "rb").read()
+    # Create bytes array from the data.
+    arr_data = [data[i:i + SIZE_PKG] for i in range(0, len(data), SIZE_PKG)]
+except:
+    off()
+
+# Add to any package the right index.
 for i in range(0, len(arr_data)):
     arr_data[i] += i.to_bytes(3, 'little')
-
+# Array for mark ack \ not ack.
 arr_ack = [False for i in range(len(arr_data))]
 
 
+# If we got ack for all packages.
 def finish():
     for sign in arr_ack:
         if not sign:
@@ -35,23 +72,26 @@ def ack():
             pkg_index = int.from_bytes(data_ack[-3:len(data_ack)], 'little')
             arr_ack[pkg_index] = True
     except:
+        # Continue send the remaining packages.
         if not finish():
             send_pkgs()
 
 
 def send_pkgs():
-    inedx = 0
+    index = 0
     for pkg in arr_data:
-        if not arr_ack[inedx]:
+        # Package with no ack - send again.
+        if not arr_ack[index]:
             c_socket.sendto(pkg, (IP, PORT))
-            time.sleep(0.1)
-        inedx += 1
+        index += 1
     ack()
 
 
 def start_net(amount):
+    # Send server amount of packages.
     c_socket.sendto(amount, (IP, PORT))
     try:
+        # Get ack and start send packages.
         ack_amount, addr = c_socket.recvfrom(3)
         send_pkgs()
     except socket.timeout:
