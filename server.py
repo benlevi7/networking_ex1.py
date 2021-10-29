3487c0cf5e32c269608dc8b631a5a4c4f2989ec4
benle <benlevi7770528@gmail.com>
2021-10-29 13:15:50 -0700

    var names

--- server.py
+++ server.py
@@ -1,36 +1,35 @@
 import socket
 import sys
 
-port = int(sys.argv[1])
+PORT = int(sys.argv[1])
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', port))
+s.bind(('', PORT))
 
-num_pak, addr = s.recvfrom(3)
-s.sendto(num_pak, addr)
-num_pak_int = int.from_bytes(num_pak, 'little')
-arr_data = [b'0'] * num_pak_int
-arr_bool = [False for i in range(num_pak_int)]
+amount, addr = s.recvfrom(3)
+s.sendto(amount, addr)
+amount_int = int.from_bytes(amount, 'little')
+arr_data = [b'0'] * amount_int
+arr_bool = [False for i in range(amount_int)]
 count = 0
 
 
 def print_data():
-    for chunk in arr_data:
-        print(chunk.decode('utf8'))
+    for pkg in arr_data:
+        print(pkg.decode('utf8'))
 
 
 while True:
-    if count == num_pak_int:
+    if count == amount_int:
         print_data()
     data, addr = s.recvfrom(100)
-    if data == num_pak:
-        s.sendto(num_pak, addr)
+    if data == amount:
+        s.sendto(amount, addr)
     else:
-        pkn_num = int.from_bytes(data[-3:len(data)], 'little')
-        # print(pkn_num)
-        if not arr_bool[pkn_num]:
-            arr_data[pkn_num] = data
-            arr_bool[pkn_num] = True
-            if count <= num_pak_int:
+        pkg_index = int.from_bytes(data[-3:len(data)], 'little')
+        if not arr_bool[pkg_index]:
+            arr_data[pkg_index] = data
+            arr_bool[pkg_index] = True
+            if count <= amount_int:
                 count += 1
         # that's why the client will stop at the end.
         s.sendto(data, addr)
